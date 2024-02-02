d9907ad85acbb35fa4d06660768c33c72e6a2fb6
LukePadawalker <padurariu.luca@gmail.com>
2024-02-02 16:55:28 +0100

    finished work, added hover animation on button

---
+++ src/components/AppButton.vue
@@ -0,0 +1,32 @@
+<script setup>
+</script>
+
+<template>
+    <button>LOAD MORE</button>
+</template>
+
+<style lang="scss" scoped>
+button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 200px;
+    height: 20px;
+    background-color: rgb(0, 106, 255);
+    border: 0;
+    padding: 20px;
+    color: white;
+    cursor: none;
+    transition: all 0.2s ease;
+    border-radius: 10px;
+
+
+
+}
+
+button:hover {
+    transform: scale(1.2);
+    background-color: rgb(90, 162, 250);
+}
+</style>
+
--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,11 +1,20 @@
 <script setup>
+import dcFilms from '/src/assets/data/dc-comics.js';
+import AppButton from './AppButton.vue'
 </script>
 
 <template>
     <div id="root">
         <div class="container">
-            <div class="card"></div>
 
+            <div class="card" v-for="film in dcFilms">
+                <div class="img-card">
+                    <img :src="film.thumb" alt="">
+                </div>
+                <p>ACTION COMICS</p>
+            </div>
+
+            <AppButton />
         </div>
 
     </div>
@@ -14,24 +23,49 @@
 <style lang="scss" scoped>
 #root {
     width: 100%;
-    background-color: rgb(20, 20, 20);
+    background-color: rgb(29, 28, 28);
     position: relative;
     z-index: 1;
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 130px;
+    min-height: 700px;
 
     .container {
         width: 75%;
         display: flex;
-        justify-content: center;
+        justify-content: space-evenly;
+        gap: 10px;
         align-items: center;
+        flex-wrap: wrap;
 
-        p {
-            width: 100%;
-            color: white;
+        .card {
+            width: calc(100% / 6 - 20px);
+            height: 250px;
+
+            .img-card {
+                width: 100%;
+                height: 180px;
+
+
+
+                img {
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+
+
+            p {
+                width: 100%;
+                color: white;
+                font-size: 0.7rem;
+                padding-top: 1rem;
+            }
+
         }
+
+
     }
 }
 </style>
